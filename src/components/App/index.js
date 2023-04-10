167ce859f6d6f53988b062812113391ebb865e48
yell0wghost <augustin.be@pm.me>
2023-04-10 18:21:54 +0200

    Create list, implement mui-theme

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,34 @@
 import * as React from "react"
 import { useTitle } from "../../hooks"
 import { Box } from "@mui/system"
+import { ThemeProvider } from '@mui/material/styles'
 import { Header } from "../"
 import "../../styles/index.scss"
-import { defaults } from "../../common"
+import { defaults, theme } from "../../common"
 
 export const App = ({ children }) => {
 
 	const { title } = useTitle()
-
+	
   return (
-    <Box
-			display='flex'
-			flexDirection='row'
-			justifyContent='center'
-		>
+		<ThemeProvider theme={theme}>
 			<Box
 				display='flex'
-				flexDirection='column'
-				width='100%'
-				height='calc(100vh - 6rem)'
-				maxWidth={defaults.layout.container.md.width}
-				padding={defaults.layout.container.md.padding}
+				flexDirection='row'
+				justifyContent='center'
 			>
-      	<Header siteTitle={title} />
-				{children}
+				<Box
+					display='flex'
+					flexDirection='column'
+					width='100%'
+					height='calc(100vh - 6rem)'
+					maxWidth={defaults.layout.container.md.width}
+					padding={defaults.layout.container.md.padding}
+				>
+					<Header siteTitle={title} />
+					{children}
+				</Box>
 			</Box>
-    </Box>
+		</ThemeProvider>
   )
 }
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,8 +1,32 @@
 import * as React from "react"
-import { Typography } from "@mui/material"
+import { Typography, List as TodoList, ListItem,
+	ListItemButton, ListItemIcon, ListItemText, Checkbox } from "@mui/material"
+
+const TodoItem = () => (
+	<ListItem
+		sx={{
+			padding: 0,
+		}}
+	>
+		<ListItemButton>
+			<ListItemIcon>
+				<Checkbox
+					color='secondary'
+					edge="start"
+					tabIndex={-1}
+					disableRipple
+				/>
+			</ListItemIcon>
+			<ListItemText id='1' primary={`Line item 1`} />
+		</ListItemButton>
+	</ListItem>
+)
 
 export const Todo = () => (
   <>
 		<Typography variant="h6">Todo's</Typography>
+		<TodoList>
+			<TodoItem/>
+		</TodoList>
 	</>
 )

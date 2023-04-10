9752ee1a70dbb254b982a51ae6845d3f6888562a
yell0wghost <augustin.be@pm.me>
2023-04-10 17:40:42 +0200

    Add more button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { IconButton } from "@mui/material"
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
 
 export const Header = ({ siteTitle }) => (
@@ -6,5 +7,14 @@ export const Header = ({ siteTitle }) => (
     <a href="#">
       {siteTitle}
     </a>
+		<IconButton
+			aria-label="display more options"
+			disableRipple
+			disableTouchRipple
+			disableFocusRipple
+			color='secondary'
+		>
+  		<MoreHorizIcon/>
+		</IconButton>
   </header>
 )
